e743ef922030340042edabd9ee5e383a04083abd
kumartul <kumartul001@gmail.com>
2022-08-27 10:26:26 +0530

    write: eye exercise content

---
+++ components/content/ContentEyeExercise.js
@@ -0,0 +1,11 @@
+const ContentEyeExercise = () => {
+	return (
+		<div>
+			<h3 className="text-lg font-bold text-gray-700 mb-4">Follow the 20-20-20 rule!</h3>
+
+			<p>Every 20 minutes, you should try to look away at something that is 20 feet away from you for a total of 20 seconds</p>
+		</div>
+	);
+}
+
+export default ContentEyeExercise;
