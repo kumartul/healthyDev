4eae7b5f3dc7aae9011172cf45961148caad6e22
kumartul <kumartul001@gmail.com>
2022-08-27 08:54:41 +0530

    feat: use ThemeToggler on every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'next-themes';
 
+import ThemeToggler from '../components/core/ThemeToggler';
+
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider attribute="class">
 			<Component {...pageProps} />
+			<ThemeToggler />
 		</ThemeProvider>
 	);
 }
