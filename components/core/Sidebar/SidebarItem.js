271ac00fbf9a80b2af1b600131b70fc5692c8d42
kumartul <kumartul001@gmail.com>
2022-08-27 09:38:09 +0530

    feat: change the base color to blue

--- components/core/Sidebar/SidebarItem.js
+++ components/core/Sidebar/SidebarItem.js
@@ -5,8 +5,8 @@ const SidebarItem = ({ href, Icon, title, isActive }) => {
 		<Link href={href}>
 			<li className={`mt-8 cursor-pointer transition-all duration-200 hover:bg-gray-200 dark:hover:bg-gray-600 p-2 rounded-lg group ${isActive && 'bg-gray-200 dark:bg-gray-600'}`}>
 				<div className="flex items-center space-x-2">
-					<Icon className={`h-8 w-8 ${isActive ? 'text-gray-900' : 'text-gray-800 dark:text-gray-300'} group-hover:text-gray-900 transition-all duration-200`} />
-					<span className={`${isActive ? 'text-gray-900' : 'text-gray-800 dark:text-gray-300'} text-semibold text-lg transition-all duration-200 group-hover:text-gray-900`}>{title}</span>
+					<Icon className={`h-8 w-8 ${isActive ? 'text-blue-700' : 'text-blue-500'} group-hover:text-blue-600 transition-all duration-200`} />
+					<span className={`${isActive ? 'text-blue-600' : 'text-blue-500'} text-semibold text-lg transition-all duration-200 group-hover:text-blue-600`}>{title}</span>
 				</div>
 			</li>
 		</Link>
