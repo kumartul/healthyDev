3f71bdc075a957adb6c8ed5c5bd1927eb0e11e30
kumartul <kumartul001@gmail.com>
2022-08-28 08:49:56 +0530

    feat: dark mode and support for custom button text

--- components/core/Modal.js
+++ components/core/Modal.js
@@ -2,12 +2,12 @@ import { Button } from '@mui/material';
 
 import { GrClose } from 'react-icons/gr';
 
-const Modal = ({ onClick, onClose, title, Content, hideCloseBtn }) => {
+const Modal = ({ onClick, onClose, title, Content, hideCloseBtn, btnText }) => {
 	return (
 		<div>
 			<div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-900 opacity-50"></div>
 			<div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
-				<div className="relative h-[40vh] w-[30vw] bg-white !opacity-100 rounded-lg z-10 p-10">
+				<div className="relative h-[40vh] w-[30vw] bg-white !opacity-100 rounded-lg z-10 p-10 dark:bg-[#111]">
 					{
 						!hideCloseBtn && (
 							<GrClose
@@ -17,7 +17,7 @@ const Modal = ({ onClick, onClose, title, Content, hideCloseBtn }) => {
 						)
 					}
 
-					<h2 className="text-gray-800 font-semibold text-2xl mb-4">{title}</h2>
+					<h2 className="text-gray-800 dark:text-gray-300 font-semibold text-2xl mb-4">{title}</h2>
 
 					<Content />
 
@@ -25,7 +25,9 @@ const Modal = ({ onClick, onClose, title, Content, hideCloseBtn }) => {
 						onClick={onClick}
 						className="!bg-blue-500 !text-white !normal-case !absolute !right-0 !bottom-0 !m-4 hover:!bg-blue-600"
 					>
-						Done
+						{
+							btnText ? btnText : 'Done'
+						}
 					</Button>
 				</div>
 			</div>
