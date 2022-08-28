063130db58e101cdc5b01bbe92bae5ef94281a13
kumartul <kumartul001@gmail.com>
2022-08-28 10:05:31 +0530

    feat: build line chart

---
+++ components/charts/LineChart.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+import { LOCAL_STORAGE_KEYS } from "../../utils/constants";
+  
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler
+);
+  
+export function LineChart() {
+	const [eyeData, setEyeData] = useState([0, 0, 0, 0, 0, 0, 0]);
+
+	useEffect(() => {
+		let eyedata = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEYS.EYE_DATA));
+		setEyeData(eyedata);
+	}, []);
+
+	const labels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: "Eye Care Exercises Analysis",
+				fill: true,
+				data: eyeData,
+				borderColor: "rgba(0, 153, 246, 0.5)",
+				backgroundColor: "rgba(0, 153, 246, 0.2)",
+				responsive: true
+			}
+		]
+	};
+
+
+	return (
+		<Line
+			data={data}
+			options={{
+				responsive: true,
+				maintainAspectRatio: false,
+			}}
+		/>
+	);
+}
+  
