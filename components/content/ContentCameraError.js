52b43c4df4285aa493564c4e84a6afe7c24bf226
kumartul <kumartul001@gmail.com>
2022-08-28 08:53:12 +0530

    feat: write camera error content

---
+++ components/content/ContentCameraError.js
@@ -0,0 +1,9 @@
+const ContentCameraError = () => {
+	return (
+		<div>
+			<h3 className="text-gray-600 dark:text-gray-300">Problem with Camera...</h3>
+		</div>
+	);
+}
+
+export default ContentCameraError;
